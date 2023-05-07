cf8ee5e48932b292e2a81c4e5f6f882b2aecaec5
Adil-debug-cpu <74548574+Adil-debug-cpu@users.noreply.github.com>
2023-05-07 15:30:42 +0530

    Update Landing.jsx

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -3,7 +3,14 @@ import UserCard from "./UserCard";
 function Landing(props) {
   const { data } = props;
 
-  // Renders
+  // This is a functional React component named Landing. It imports React and a custom UserCard component. The component takes in props as a parameter, and from the props, it destructures data.
+
+//The component then returns a fragment (<> and </>) that contains an unordered list (<ul> and </ul>) that is dynamically populated with data from the data prop. It maps over each user in the data array and creates a new UserCard component for each one, passing in various properties such as userId, avatar, login, and contributions.
+
+//The key prop is also passed to each UserCard component to help React optimize the rendering process.
+
+//Once all the UserCard components have been created, they are added to the unordered list and returned by the Landing component.
+  
   return (
     <>
       <ul>
