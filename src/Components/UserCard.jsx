5266d7258deb3c840b29947bed2f8e2257e1a068
Adil-debug-cpu <74548574+Adil-debug-cpu@users.noreply.github.com>
2023-05-07 15:34:14 +0530

    Update UserCard.jsx

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+//UserCard
+
+//This is a React functional component that renders a user card. It takes in props such as userId, avatar, login, and contributions to display relevant user data.
+
+//It uses the Link component from react-router-dom to link to a specific user's detail page based on the userId prop.
+
+//The component then returns a list item with an image container and an information container, which displays the user's login and contributions.
+
 function UserCard(props) {
-  // Grab relevent data from props to display
   const { userId, avatar, login, contributions } = props;
 
-  // Renders
   return (
     <Link to={`/user-detail/${userId}`} state={props}>
       <li>
